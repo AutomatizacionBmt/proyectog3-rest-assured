9e4d722a1783cad78bcaf33ddf9a3e58fde8ba0f
Jose Hurtado <josehumberto17@gmail.com>
2020-10-17 12:04:19 -0500

    Add new tests

---
+++ src/test/java/com/company/config/RedmineEndpoints.java
@@ -0,0 +1,10 @@
+package com.company.config;
+
+public class RedmineEndpoints {
+
+    public static final String REDMINE_ISSUES_JSON = "issues.json";
+    public static final String SINGLE_REDMINE_ISSUE_JSON = "issues/{idIssue}.json";
+
+    public static final String SINGLE_REDMINE_ISSUE_XML = "issues/{idIssue}.xml";
+
+}
--- src/test/java/com/company/features/RedmineTest.java
+++ src/test/java/com/company/features/RedmineTest.java
@@ -1,6 +1,7 @@
 package com.company.features;
 
 import com.company.config.RedmineConfig;
+import com.company.config.RedmineEndpoints;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -22,9 +23,66 @@ public class RedmineTest extends RedmineConfig {
         given()
                 .body(issueBody).
         when()
-                .post("issues.json").
+                .post(RedmineEndpoints.REDMINE_ISSUES_JSON).
         then()
                 .statusCode(201);
     }
+
+    @Test
+    public void getSingleIssueJSON(){
+
+        given()
+                .pathParam("idIssue",1020).
+        when()
+                .get(RedmineEndpoints.SINGLE_REDMINE_ISSUE_JSON).
+        then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void updateIssueJSON(){
+
+        String issueBody = "{\n" +
+                "  \"issue\": {\n" +
+                "    \"subject\": \"Issue Modificado por José Humberto\",\n" +
+                "    \"priority_id\" : 5,\n" +
+                "    \"notes\": \"The subject was changed\" \n" +
+                "  }\n" +
+                "}";
+
+        given()
+                .body(issueBody)
+                .pathParam("idIssue", 1020).
+        when().
+                put(RedmineEndpoints.SINGLE_REDMINE_ISSUE_JSON).
+        then()
+                .statusCode(204);
+
+    }
+
+    @Test
+    public void deleteIssue(){
+
+        given().
+        when()
+                .delete("issues/1022.json").
+        then()
+                .statusCode(204);
+    }
+
+    @Test
+    public void getSingleIssueXML(){
+
+        given()
+                .contentType("application/xml")
+                .pathParam("idIssue", 1019).
+        when()
+                .get(RedmineEndpoints.SINGLE_REDMINE_ISSUE_XML).
+        then()
+                .statusCode(200);
+
+    }
+
+
 
 }
